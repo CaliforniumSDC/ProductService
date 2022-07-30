e0267d8dbc9821df85dddf67f3fd21a16aac777f
chenyou <chenyouhuang@gmail.com>
2022-07-29 20:15:06 -0400

    Refactor server to increase code readability

--- server/server.js
+++ server/server.js
@@ -5,24 +5,21 @@ const app = express();
 const port = 3000;
 let db;
 
-// Initialize connection once
-MongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true }, (err, database) => {
-  if (err) throw err;
+// ================== Server Functions ==================//
 
-  db = database.db('betterSDC');
-
-  // Start the application after the database connection is ready
-  app.listen(3000);
-  console.log(`Listening on port http://localhost:${port}`);
-});
-
-function getProducts(productDetail, page, count) {
+// ------------------ getData ------------------ //
+function findProducts(productDetail, page, count) {
   let reqestAmount = page * count;
   // prevent user request to many data
   reqestAmount = reqestAmount <= 200 ? reqestAmount : 20;
   return productDetail.find().limit(reqestAmount).toArray();
 }
 
+function findRelatedProducts(relatedProduct, productId) {
+  return relatedProduct.find({ current_product_id: productId }).limit(48).toArray();
+}
+
+// ------------------ transformData ------------------ //
 function transformProducts(products) {
   const result = [];
   products.forEach((product) => {
@@ -37,12 +34,58 @@ function transformProducts(products) {
   });
   return result;
 }
+
+function transformRelatedProducts(products) {
+  const relatedProducts = [];
+  products.forEach((product) => {
+    relatedProducts.push(product.related_product_id);
+  });
+  return relatedProducts;
+}
+
+function transformStyles(styles) {
+  const results = [];
+  const finalStyle = {};
+  styles.forEach((style) => {
+    const newStyleFormat = {};
+    newStyleFormat.style_id = style.id;
+    newStyleFormat.name = style.name;
+    newStyleFormat.original_price = style.original_price;
+    newStyleFormat.sale_price = (style.sale_price === 'null' ? 0 : style.sale_price);
+    newStyleFormat['default?'] = (style.default_style === 1);
+    newStyleFormat.skus = {};
+    style.skus.forEach((sku) => {
+      newStyleFormat.skus[sku.id] = {};
+      newStyleFormat.skus[sku.id].quantity = sku.quantity;
+      newStyleFormat.skus[sku.id].size = sku.size;
+    });
+    newStyleFormat.photos = style.photos;
+    results.push(newStyleFormat);
+    finalStyle.product_id = style.productId;
+    // stylesOutput.push(finalStyle);
+  });
+  finalStyle.results = results;
+  return finalStyle;
+}
+
+// ================== Server Router & MongoDB Connect ==================//
+// Initialize connection once
+MongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true }, (err, database) => {
+  if (err) throw err;
+
+  db = database.db('betterSDC');
+
+  // Start the application after the database connection is ready
+  app.listen(3000);
+  console.log(`Listening on port http://localhost:${port}`);
+});
+
 // Reuse database object in request handlers
 app.get('/products', (req, res) => {
   const page = req.query.page || 1;
   const count = req.query.count || 5;
-  const productDetail = db.collection('product_detail');
-  getProducts(productDetail, page, count)
+  const productDetailColl = db.collection('product_detail');
+  findProducts(productDetailColl, page, count)
     .then((products) => {
       res.send(transformProducts(products));
     })
@@ -54,8 +97,8 @@ app.get('/products', (req, res) => {
 
 app.get('/products/:product_id', (req, res) => {
   const pId = parseInt(req.params.product_id, 10);
-  const productDetail = db.collection('product_detail');
-  productDetail
+  const productDetailColl = db.collection('product_detail');
+  productDetailColl
     .findOne({ id: pId })
     .then((products) => {
       res.send(products);
@@ -67,18 +110,11 @@ app.get('/products/:product_id', (req, res) => {
 });
 
 app.get('/products/:product_id/related', (req, res) => {
-  const relatedProducts = [];
   const pId = parseInt(req.params.product_id, 10);
-  const productDetail = db.collection('related');
-  productDetail
-    .find({ current_product_id: pId })
-    .limit(48)
-    .toArray()
-    .then((products) => {
-      products.forEach((product) => {
-        relatedProducts.push(product.related_product_id);
-      });
-      res.send(relatedProducts);
+  const relatedProductColl = db.collection('related');
+  findRelatedProducts(relatedProductColl, pId)
+    .then((relatedProducts) => {
+      res.send(transformRelatedProducts(relatedProducts));
     })
     .catch((error) => {
       res.sendStatus(500);
@@ -93,28 +129,7 @@ app.get('/products/:product_id/styles', (req, res) => {
     .find({ productId: pId })
     .toArray()
     .then((styles) => {
-      const results = [];
-      const finalStyle = {};
-      styles.forEach((style) => {
-        const newStyleFormat = {};
-        newStyleFormat.style_id = style.id;
-        newStyleFormat.name = style.name;
-        newStyleFormat.original_price = style.original_price;
-        newStyleFormat.sale_price = (style.sale_price === 'null' ? 0 : style.sale_price);
-        newStyleFormat['default?'] = (style.default_style === 1);
-        newStyleFormat.skus = {};
-        style.skus.forEach((sku) => {
-          newStyleFormat.skus[sku.id] = {};
-          newStyleFormat.skus[sku.id].quantity = sku.quantity;
-          newStyleFormat.skus[sku.id].size = sku.size;
-        });
-        newStyleFormat.photos = style.photos;
-        results.push(newStyleFormat);
-        finalStyle.product_id = style.productId;
-        // stylesOutput.push(finalStyle);
-      });
-      finalStyle.results = results;
-      res.send(finalStyle);
+      res.send(transformStyles(styles));
     })
     .catch((error) => {
       res.sendStatus(500);
